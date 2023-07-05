5e08f43bb2cfa71e2adb8bb71a8a1593b850cda8
abdelali-elhammadi-nw <abdelali.el-hammadi@nimbleways.com>
2023-07-05 15:12:40 +0100

    Release v1.0.0

---
+++ src/main/java/com/nimbleways/odinzeye/datacollector/databasequerycollector/JpaMethodsCollector.java
@@ -0,0 +1,43 @@
+package com.nimbleways.odinzeye.datacollector.databasequerycollector;
+
+
+import com.nimbleways.odinzeye.websocket.IWSDispatcher;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class JpaMethodsCollector {
+
+    @Autowired
+    private  DataBaseQueryEntity dataBaseQueryEntity;
+
+    private final IWSDispatcher wsDispatcher;
+    public JpaMethodsCollector(final IWSDispatcher wsDispatcher){
+
+        this.wsDispatcher = wsDispatcher;
+    }
+
+    @Around("execution(* org.springframework.data.jpa.repository.JpaRepository+.*(..))  && target(repository)")
+    public Object interceptJpaRepositoryMethods(ProceedingJoinPoint joinPoint, JpaRepository<?,?> repository) throws Throwable {
+        dataBaseQueryEntity.setDispatchedFromJPA(true);
+
+        Object result = joinPoint.proceed();
+
+        String methodName = joinPoint.getSignature().getName();
+        String className = repository.getClass().getInterfaces()[0].getSimpleName();
+        dataBaseQueryEntity.setMethodName(methodName);
+        dataBaseQueryEntity.setClassName(className);
+        dataBaseQueryEntity.setQueryResult(result);
+
+        wsDispatcher.sendCollectedDBQueries(dataBaseQueryEntity);
+        dataBaseQueryEntity.setSql(null);
+
+        return result;
+    }
+
+}
